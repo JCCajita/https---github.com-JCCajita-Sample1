8235af519e2b039436556e4ab5518f9c68189224
JC Cajita <jc.cajita@liston.school.nz>
2022-06-07 10:46:15 +1200

    First Commit

---
+++ 01_yes_no_v1.py
@@ -0,0 +1,13 @@
+show_instructions = "hi"
+
+while show_instructions == "hi":
+    show_instructions = input("have you played this game before? ").lower()
+
+    if show_instructions == "yes" or show_instructions == "y":
+        print("program continues")
+    elif show_instructions == "no" or show_instructions == "n":
+        print("display instructions")
+    else:
+        print("yes or no")
+
+print("hi")
